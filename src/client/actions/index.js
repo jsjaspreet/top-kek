0c083becc1f39310eebb8986546fb355183afd37
Jaspreet Singh <jaspreet_xps@yahoo.com>
2016-11-24 20:03:56 -0800

    Update production api location

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -9,7 +9,7 @@ export function getMemeLinks() {
     promise = axios.get(`/api/memes`)
   }
   else {
-    promise = axios.get(`https://138.68.58.61:5050/api/memes`)
+    promise = axios.get(`https://topkekkle:5050/api/memes`)
   }
   return {
     type: GET_MEME_LINKS,
